1802eb935ab7620832644fb0863f39a7c7ab1f37
nikitasmall <nikitasosnov@yahoo.com>
2015-07-29 15:41:12 +0300

    little tests with dummy rails app

--- lib/generators/pretty_weather_2/install_generator.rb
+++ lib/generators/pretty_weather_2/install_generator.rb
@@ -1,9 +1,10 @@
-require 'Rails::Generators'
+require 'rails' if defined?(Rails)
 
 module PrettyWeather2
-  module Generators
+  module Generators # doesn't work. I haven't time (as this is additional task and nobody talks about it)
+    # but it registered
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path("../../templates", __FILE__)
+      source_root File.expand_path("../templates", __FILE__)
       desc "Creates PrettyWeather2 initializer for your application"
 
       def copy_initialize
--- spec/dummy/config/initializers/pretty_weather_2.rb
+++ spec/dummy/config/initializers/pretty_weather_2.rb
@@ -1,12 +1,12 @@
 PrettyWeather2.configure do |config|
   config.city = 'Odesa'
-  config.data_provider = :open_weather
+  config.data_provider = :world_weather
   config.fallback_provider = :optimistic_weather
   config.latitude = 46.482526
   config.longitude = 30.723310
   config.units = :metric
 
   # to manage keys:
-  config.forecast_api_key = 'your forecast.io api key'
-  config.world_weather_api_key = 'your WorldWeatherOnline api key'
+  config.forecast_api_key = 'da01296688e16554f19b3161f69f158f'
+  config.world_weather_api_key = '2a034b1c63a5b6fec14c891fbe02d'
 end
